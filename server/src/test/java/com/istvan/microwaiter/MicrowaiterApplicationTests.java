be2a5895397b13c110dd22ea915a3a0063da1a98
IstvanM <istvan.meszaros.88@gmail.com>
2016-11-01 11:33:24 +0100

    Server project added

---
+++ server/src/test/java/com/istvan/microwaiter/MicrowaiterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.istvan.microwaiter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrowaiterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
